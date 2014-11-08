3d45b291a85ef6fa2ecaa24991766e00f152c71d
jbahus <julesbahus@hotmail.fr>
2014-11-08 23:10:15 +0100

    ft_memcmp fonctionnel

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jbahus <jbahus@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2014/11/05 16:47:13 by jbahus            #+#    #+#             */
-/*   Updated: 2014/11/08 21:40:30 by jbahus           ###   ########.fr       */
+/*   Created: 2014/11/08 22:41:07 by jbahus            #+#    #+#             */
+/*   Updated: 2014/11/08 23:06:32 by jbahus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,17 @@
 
 int		ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	const char	*str1;
-	const char	*str2;
+	unsigned char	*str1;
+	unsigned char	*str2;
 
-	str1 = s1;
-	str2 = s2;
-	while (*str1++ == *str2++ && *str1 && n > 0 && *str1)
-		n--;
-	return (*str1 - *str2);
+	while (n--)
+	{
+		str1 = (unsigned char*)s1;
+		str2 = (unsigned char*)s2;
+		if (*str1 != *str2)
+			return (*str1 - *str2);
+		s1++;
+		s2++;
+	}
+	return (0);
 }
