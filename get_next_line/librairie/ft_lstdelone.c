a7a89c6e18c2196e8d027233a86c0daa1c904134
jbahus <julesbahus@hotmail.fr>
2014-11-15 19:25:58 +0100

    lot of things

---
+++ get_next_line/librairie/ft_lstdelone.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbahus <jbahus@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/11/12 04:53:59 by jbahus            #+#    #+#             */
+/*   Updated: 2014/11/13 04:17:22 by jbahus           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstdelone(t_list **alst, void (*del)(void *, size_t))
+{
+	t_list	*elem;
+
+	elem = *alst;
+	(*del)(elem->content, elem->elem_size);
+	ft_memdel(alst);
+}
