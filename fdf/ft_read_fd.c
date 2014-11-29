d40c87fdc34fdbabf5a4b81150fed64fcb2f86b4
jbahus <julesbahus@hotmail.fr>
2014-11-30 00:14:13 +0100

    modif

--- fdf/ft_read_fd.c
+++ fdf/ft_read_fd.c
@@ -6,12 +6,15 @@
 /*   By: jbahus <jbahus@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/26 19:55:24 by jbahus            #+#    #+#             */
-/*   Updated: 2014/11/29 03:46:57 by jbahus           ###   ########.fr       */
+/*   Updated: 2014/11/30 00:13:01 by jbahus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 #include <stdio.h>
+
+t_stock	*stock__stock(char **ret, t_stock *prev);
+t_stock	*node_create(int data, t_stock *prev);
 
 int		open_f(char *fname)
 {
@@ -36,25 +39,72 @@ int		open_f(char *fname)
 	return (0);
 }
 
+/*void	ft_placeEOS(t_stock **end, t_stock *s)
+{
+	while (s && s->next)
+		s = s->next;
+	s->next = *end;
+}*/
+
 void	ft_create_tab(char *line)
 {
-	char	**ret;
-	int 	i;
+	char		**ret;
+	t_stock		*s;
 
-	i = 0;
 	ret = (char**)ft_strnew(ft_strlen(line));
 	ret = ft_strsplit(line, ' ');
-	while(*ret != '\0' && i < 19)
+	ft_putendl(*ret);
+	s = node_create(ft_atoi(*ret), NULL);
+	ret++;
+	while(*ret != '\0')
 	{
-		i = 0;
-		printf("%s", *ret);
+//		printf("%s", *ret);
+		//s = (t_stock*)ft_memalloc(sizeof(t_stock));
+		//s->nb = ft_atoi(*ret);
+		//s = s->next;
+		s = node_create(ft_atoi(*ret), s);
 		ret++;
+//		ret++;
 	}
 	printf("\n");
-	/*while (ret)
-	{
-		while (*ret)
-			*tab[i++] = ft_atoi(*ret++);
-		ret++;
-	}*/
 }
+
+/*t_stock	*stock__stock(char **ret, t_stock *prev)
+{
+	if((ret != NULL))
+       {
+           t_stock *begin;
+           begin = node_create(ft_atoi(*ret), prev);
+           stock__stock(++ret, begin);
+       }
+    else if(ret == NULL)
+       return (prev);
+   return (prev);
+}*/
+       
+t_stock	*node_create(int data, t_stock *prev)
+    {
+        t_stock	*temp;
+        
+        temp = (t_stock*)malloc(sizeof(t_stock));
+        temp->nb = malloc(1);
+        
+        if (temp == '\0')
+        {
+            ft_putendl("Malloc failed (temp)\n");
+            exit(0);
+        }
+        if (temp->nb == '\0')
+        {
+            ft_putendl("Malloc failed (data)\n");
+            exit(0);
+        }
+        
+		temp->nb = data;
+        
+        if (prev != NULL)
+            prev->next = temp;
+		temp->next = NULL;
+        
+        return (temp);
+    }
