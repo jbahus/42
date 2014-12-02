4ecc887eb70c4ff6534b12a27a29ffb5fa53b0fd
jbahus <julesbahus@hotmail.fr>
2014-12-02 16:49:28 +0100

    stockage des caractere, DONE.

--- fdf/ft_read_fd.c
+++ fdf/ft_read_fd.c
@@ -6,33 +6,41 @@
 /*   By: jbahus <jbahus@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/26 19:55:24 by jbahus            #+#    #+#             */
-/*   Updated: 2014/12/01 22:44:29 by jbahus           ###   ########.fr       */
+/*   Updated: 2014/12/02 16:48:43 by jbahus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-t_stock	*ft_create_lst(int nb, t_stock *lst)
+t_stock	*ft_create_lst(int nb, char nl, t_stock *lst)
 {
 	t_stock *new_lst;
 
-	new_lst = (t_stock*)malloc(sizeof(t_stock));
-	new_lst->nb = nb;
-	new_lst->prev = lst;
-	lst->next = new_lst;
+	if (!lst->prev && !lst->next)
+	{
+		lst = (t_stock*)malloc(sizeof(t_stock));
+		lst->prev = NULL;
+		lst->nb = nb;
+	}
+	else
+	{
+		new_lst = (t_stock*)malloc(sizeof(t_stock));
+		new_lst->nb = nb;
+		new_lst->nl = nl;
+		new_lst->prev = lst;
+		lst->next = new_lst;
+	}
 	return (new_lst);
 }
 
 t_stock *ft_return_bol(t_stock *lst)
 {
-	static int i;
-
-	i = 0;
 	while (lst->prev != NULL)
 	{
-		ft_putnbr(i);
-		ft_putchar('\n');
-		i++;
+		if (lst->nl == '\n')
+			ft_putchar('\n');
+		else
+			ft_putnbr(lst->nb);
 		lst = lst->prev;
 	}
 	return (lst);
@@ -44,11 +52,10 @@ t_stock	*ft_go_lst(char *line, t_stock *lst)
 
 	ret = (char**)ft_strnew(ft_strlen(line));
 	ret = ft_strsplit(line, ' ');
-	lst->nb = ft_atoi(*ret);
-	ret++;
-	while (*ret != NULL)
+	while (*ret)
 	{
-		lst = ft_create_lst(ft_atoi(*ret), lst);
+		lst = ft_create_lst(ft_atoi(*ret), '\0', lst);
+		free(*ret);
 		ret++;
 	}
 	return (lst);
@@ -58,21 +65,19 @@ int		open_f(char *fname)
 {
 	int		fd;
 	char	*line;
-	int 	ret;
+	int		ret;
 	t_stock *lst;
 
 	fd = open(fname, O_RDONLY);
 	if (fd == -1)
 		ft_error(fname);
 	line = NULL;
-	lst = (t_stock*)malloc(sizeof(t_stock));
-	lst->prev = NULL;
 	while ((ret = get_next_line(fd, &line)))
 	{
 		if (ret == -1)
 			ft_error(fname);
 		lst = ft_go_lst(line, lst);
-		lst = ft_create_lst('\n', lst);
+		lst = ft_create_lst('\0', '\n', lst);
 		free(line);
 	}
 	ft_return_bol(lst);
